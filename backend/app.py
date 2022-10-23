d7df0f4b64e560a39851a61c20ae8085da4c4d91
Anshaal Hussain <anshaalhussain@gmail.com>
2022-10-23 09:52:22 -0400

    create initial api routes

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,21 @@
-from flask import Flask
+from flask import Flask, request
 from views import views
 
 app = Flask(__name__)
-app.register_blueprint(views, url_prefix="/")
+
+
+@app.route("/")
+def home():
+    return "this isnt not the home page"
+
+
+@app.route("/data", methods=['GET', 'POST'])
+def data():
+    if request.method == 'POST':
+        return "this is the data page"
+    else:
+        return "this isn't the data page"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
