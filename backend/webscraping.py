57497db803afd3957806ec24c4d08adec88c6c0d
Anshaal Hussain <anshaalhussain@gmail.com>
2022-11-16 07:00:07 -0500

    use selenium for webscraping site

--- backend/webscraping.py
+++ backend/webscraping.py
@@ -1,6 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-website = 'https://incidecoder.com/products/canmake-mermaid-skin-gel-uv-spf-50-pa-01-clear'
+website = 'https://incidecoder.com/'
 
 path = '/Users/anshaal/Downloads/chromedriver'
 
@@ -8,11 +13,33 @@ driver = webdriver.Chrome(path)
 
 driver.get(website)
 
-matches = driver.find_elements_by_class_name("hashtag")
+# find html tag for search bar
+search = driver.find_element_by_id("query")
 
-print("matches", matches)
+# search query
+search.send_keys("canmake")
 
-for match in matches:
-    print(match.text.split())
+# have enter key pressed
+search.send_keys(Keys.RETURN)
 
-driver.quit()
+# wait for page to load
+# try:
+#     main = WebDriverWait(driver, 10).until(
+#         EC.presence_of_element_located((By.CLASS_NAME, "detailpage"))
+#     )
+# finally:
+#     driver.quit()
+
+main = driver.find_element_by_class_name("detailpage")
+print(main.text)
+
+# matches = driver.find_elements_by_class_name("hashtag")
+
+# print("matches", matches)
+
+# for match in matches:
+#     print(match.text.split())
+
+# time.sleep(5)
+
+# driver.quit()
