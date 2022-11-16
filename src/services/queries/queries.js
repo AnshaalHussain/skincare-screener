6c8beebbd8e4a587a2002492d3469f81cc73f653
Anshaal Hussain <anshaalhussain@gmail.com>
2022-11-16 07:03:20 -0500

    create axios utility functions file

--- src/services/queries/queries.js
+++ src/services/queries/queries.js
@@ -3,8 +3,7 @@ import axios from "axios";
 const url = "http://127.0.0.1:5000";
 
 const getHomePage = async () => {
-  const data = await axios.get(url);
-
+  const data = await axios.get(url + "/data");
   return data;
 };
 
