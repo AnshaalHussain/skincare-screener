0d0f7f43e3c1fcb1763f229df0476e31a538a53b
Anshaal Hussain <anshaalhussain@gmail.com>
2022-12-09 02:43:43 -0500

    add argument --disable gpu

--- backend/webscraping.py
+++ backend/webscraping.py
@@ -42,6 +42,7 @@ def product_search(search_query):
     chromeOptions = Options()
     chromeOptions.headless = True
     chromeOptions.binary_location = "chromedriver"
+    chromeOptions.add_argument("--disable-gpu")
     chromeOptions.add_argument("--window-size=1920,1080")
     chromeOptions.add_argument(
         'user-agent="MQQBrowser/26 Mozilla/5.0 (Linux; U; Android 2.3.7; zh-cn; MB200 Build/GRJ22; CyanogenMod-7) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1"')
