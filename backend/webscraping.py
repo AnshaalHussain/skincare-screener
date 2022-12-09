1e66ad640a42f8861d4c407b4834f20f4ba230c7
Anshaal Hussain <anshaalhussain@gmail.com>
2022-12-09 03:57:40 -0500

    changing file path

--- backend/webscraping.py
+++ backend/webscraping.py
@@ -49,7 +49,7 @@ def product_search(search_query):
     chromeOptions.add_argument(
         'user-agent="MQQBrowser/26 Mozilla/5.0 (Linux; U; Android 2.3.7; zh-cn; MB200 Build/GRJ22; CyanogenMod-7) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1"')
 
-    chromeOptions.binary_location = "chromedriver.exe"
+    chromeOptions.binary_location = "/chromedriver.exe"
 
     driver = webdriver.Chrome(
         ChromeDriverManager().install(), options=chromeOptions)
