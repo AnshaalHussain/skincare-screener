fb845f9b46ff8ba14ead88eb7832ecc7458243d0
Anshaal Hussain <anshaalhussain@gmail.com>
2023-05-12 03:02:16 -0400

    install react-router, create initial routes

---
+++ skincare-screener/src/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDGmtLbcMjU_-robe4pRDdiiAXH2SHwk30",
+  authDomain: "authskincare-b0674.firebaseapp.com",
+  projectId: "authskincare-b0674",
+  storageBucket: "authskincare-b0674.appspot.com",
+  messagingSenderId: "986172512353",
+  appId: "1:986172512353:web:2b22e961642c4c41a1f220",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
